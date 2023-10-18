ab3d8962d4b73edf3862b23ef325eaa524308ff5
jus1234 <osjamg1996@gmail.com>
2023-10-18 20:50:29 +0900

    prog77485

---
+++ prog77485.py
@@ -0,0 +1,43 @@
+def solution(rows, columns, queries):
+    answer = []
+
+    matrix = [[(i) * columns + (j + 1) for j in range(columns)] for i in range(rows)]
+    for x1, y1, x2, y2 in queries:
+        answer.append(rotate(x1 - 1, y1 - 1, x2 - 1, y2 - 1, matrix))
+
+    return answer
+
+def rotate(x1, y1, x2, y2, matrix):
+    first = matrix[x1][y1]
+    min_value = first
+
+    #왼쪽
+    for k in range(x1, x2):
+        matrix[k][y1] = matrix[k + 1][y1]
+        min_value = min(min_value, matrix[k + 1][y1])
+
+    #아래쪽
+    for k in range(y1, y2):
+        matrix[x2][k] = matrix[x2][k + 1]
+        min_value = min(min_value, matrix[x2][k + 1])
+    
+    #오른쪽
+    for k in range(x2, x1, -1):
+        matrix[k][y2] = matrix[k - 1][y2]
+        min_value = min(min_value, matrix[k - 1][y2])
+
+    #위쪽
+    for k in range(y2, y1 + 1, -1):
+        matrix[x1][k] = matrix[x1][k - 1]
+        min_value = min(min_value, matrix[x1][k - 1])
+
+    matrix[x1][y1 + 1] = first
+    print(matrix)
+    return min_value
+
+
+print(solution(6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]])) #[8, 10, 25]
+
+
+
+
