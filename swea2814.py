fdd63f48544ecb6320177d94510ffb510dfd2b71
jus1234 <osjamg1996@gmail.com>
2023-11-09 16:44:26 +0900

    11/9

--- swea2814.py
+++ swea2814.py
@@ -1,28 +1,27 @@
-def dfs(fr):
-    global graph, visited, now
-    visited[fr]  = True
-    now += 1
-    for to in range(1, N + 1):
-        if graph[fr][to] and not visited[to]:
-            dfs(to)
+def dfs(i, v):
+    global now
+    now = max(now, len(v))
+    for j in graph[i]:
+        if j not in v:
+            dfs(j, v + [j])
+                
 
 T = int(input())
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
+    graph = [[] for _ in range(0, N + 1)]
 
-    graph = [[False] * (N + 1) for _ in range(N + 1)]
-    visited = [False] * (N + 1)
-
-    for i in range(0, M):
-        fr, to = map(int, input().split())
-        graph[fr][to] = True
-        graph[to][fr] = True
+    for i in range(M):
+        x, y = map(int, input().split())
+        graph[x].append(y)
+        graph[y].append(x)
 
     mx = 0
     for i in range(1, N + 1):
-        if not visited[i]:
-            now = 0
-            dfs(i)
-            mx = max(now, mx)
-
+        now = 0
+        dfs(i, [i])
+        mx = max(now, mx)
     print(f'#{test_case} {mx}')
+    
+
+    
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 a = [1,2,3,4,5]
-print(sum(a[1:2]))
+print(118 in a)
