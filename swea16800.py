48e27c4d48afac39121266a9a33ee3a285d9a170
jus1234 <osjamg1996@gmail.com>
2023-11-19 09:54:40 +0900

    11/19

--- swea16800.py
+++ swea16800.py
@@ -1,13 +1,8 @@
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
-    x = 0
-    y = 0
     for i in range(int(N**(1/2) + 1), 0 , -1):
         if N % i == 0:
-            x = i
-            y = N // i
-            break
-
-    print(f'#{test_case} {x - 1 + y - 1}')
+            print(f'#{test_case} {i - 1 + (N // i) - 1}')
+            break    
 
