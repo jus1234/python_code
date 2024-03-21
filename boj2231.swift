96eb6297cd837b3572e7726d88990276d6b65081
jus1234 <osjamg1996@gmail.com>
2024-03-21 19:40:41 +0900

    3/21

--- boj2231.swift
+++
@@ -1,9 +0,0 @@
-let N = Int(readLine()!)!
-var res: Int = 0
-for i in 1...N {
-    if N == i + String(i).compactMap({ Int(String($0)) }).reduce(0, +) {
-        res = i
-        break
-    }
-}
-print(res)
