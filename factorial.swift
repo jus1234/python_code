cf2eaa760f966a22461b42e4393debc5790de7c0
jus1234 <osjamg1996@gmail.com>
2024-03-23 17:09:29 +0900

    3/23

--- factorial.swift
+++
@@ -1,7 +0,0 @@
-func factorial(_ n: Int) {
-	if n == 1 { // base case
-		return 1
-	} else { // recursive case
-		return n * factorial(n - 1)
-	}
-}
--- max.py
+++
@@ -1,7 +0,0 @@
-def max(a, b, c):
-    maximum = a
-    if maximum < b : maximum = b
-    if maximum < c : maximum = c
-    return maximum
-
-print(f'max(1,2,3) = {max(1,2,3)}')
