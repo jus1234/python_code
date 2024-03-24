993e1a0571775c666daa07548c20082d9308af59
jus1234 <osjamg1996@gmail.com>
2024-03-24 16:42:18 +0900

    3/24

--- boj1181.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-lst = []
-for _ in range(N):
-    lst.append(input())
-lst = set(lst)
-lst = sorted(list(lst), key=lambda x:(len(x), x))
-
-for l in lst:
-    print(l)
--- swea14692.py
+++
@@ -1,4 +0,0 @@
-T = int(input())
-for test_case in range(1, T + 1):
-    N = int(input())
-    print(f'#{test_case} {"Alice" if N % 2 == 0 else "Bob"}')
